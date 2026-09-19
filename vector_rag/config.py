43d7a2a74a7bf4b8da341111356ab8a7b6d4887b
Yash Bhurke <yashbhurke69@gmail.com>
2026-09-19 19:52:39 +0530

    completed vector_rag + trimed redundencies

--- vector_rag/config.py
+++
@@ -1,9 +0,0 @@
-import os
-from pathlib import Path
-
-API_KEY = os.getenv("NVIDIA_API_KEY")
-HF_TOKEN = os.getenv("HF_TOKEN")
-EVAL_API_KEY = os.getenv("EVAL_API_KEY")
-
-CACHE_DIR = Path("../cache")
-CACHE_DIR.mkdir(exist_ok=True)
